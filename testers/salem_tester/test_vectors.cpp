d3fdbe202d94687aa76215bbd670be7764035f03
Salem-1 <ph.ahmedsalem@gmail.com>
2023-03-23 06:15:23 -0400

    performed test for at, back and front

--- testers/salem_tester/test_vectors.cpp
+++ testers/salem_tester/test_vectors.cpp
@@ -3,12 +3,34 @@
 #include <vector>
 #include <stddef.h>
 
+void	test_at()
+{
+	std::vector<int> original;
+	ft::vector<int>	mine;
+
+	for (int i = 0; i < 100; i++)
+	{
+		original.push_back(i);
+		mine.push_back(i);
+	}
+	for (int i = 0; i < 100 ; i++)
+	{
+		std::cout << "at original " << original.at(i) << std::endl;			
+		std::cout << "at mine " << mine.at(i) << std::endl;			
+		
+	}	
+	std::cout << "back original " << original.back() << " front ";
+	std::cout << original.front() << std::endl;	
+	std::cout << "back mine " << mine.back() << " front ";
+	std::cout << mine.front() << std::endl;
+}
+
 int main()
 {
 	std::cout << "Testing capcity" << std::endl;
-	std::vector<int> a;
+	ft::vector<int> a;
 	a.back();
-	// std::cout <<  a.back() << std::endl;
+	std::cout <<  a.back() << std::endl;
 	std::cout << "yes empty " << a.empty() << std::endl; 
 	std::cout << "NO empty " << a.empty() << std::endl; 
 	// for (int i = 0; i < 100000; i++)
@@ -16,6 +38,7 @@ int main()
 	// 	a.push_back(i);
 	// 	std::cout <<  a[i] << " capacity = " << a.capacity() <<std::endl;
 	// }
+	test_at();
 	return 0;
 
 }
