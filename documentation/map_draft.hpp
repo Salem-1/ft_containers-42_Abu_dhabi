72781b0f55f6444faa506ad10227bc6bd1a18c62
Ahmed Salem <ahsalem@lab1r4s9.42abudhabi.ae>
2023-04-09 04:15:22 +0400

    still studying the trees

--- documentation/map_draft.hpp
+++ documentation/map_draft.hpp
@@ -156,4 +156,159 @@ tree	*find_maximum(tree *btree)
 	return (btree);
 }
 
-//insertion
+//insertion 
+//assume that the node is fill with key with the left and right = NULL
+
+void	insert_in_tree(tree **T, tree *node)
+{
+	tree	*tmp = T.root; //tmp = root
+	tree	*node_parent;
+	//find location to insert node in
+	while (tmp != NULL)
+	{
+		node_parent = tmp;
+		if (node.key < tmp.key)
+			tmp = tmp->left;
+		else
+			tmp = tmp->right;
+	}
+	node.parent = node_parent;
+	if (node_parent != NULL)
+		T.root = node;      // the tree was empty
+	else if (node.key < node_parent.key)
+		node_parent->left = node;
+	else
+		node_parent->right = node;
+}
+
+// transplantation
+void	transplant(tree **T, old, tree *new_node)
+{
+	tree *root = *T;
+
+	if (old.parent == NULL) //old is the root of the tree
+		T.root = new_node; 
+	else if (old == old.parent->left) //old is the left child of it's parent
+		old.parent->left = new_node;
+	else  //old is left child of it's parent
+		old.parent->right = new_node;
+	if (new_node != NULL)
+		new_node.parent = old.parent;
+}
+//transplant is not responsible for updating new_node left and right
+
+
+//deleting node 😑
+void	tree_delete(tree **T, tree *delete_me)
+{
+	//delete_me has no left child
+	if (delete_me->left == NULL)
+		Transplant(T, delete_me, delete_me->right);
+	//delete_me has no right child
+	else if (delete_me->right == NULL)
+		Transplant(T, delete_me, delete_me->left);
+	else
+	{//delete me has 2 children
+		tree *successor = tree_minimum(delete_me->right);
+		if (successor != delete_me->right)
+		{
+			//replase successor with it's right child
+			transplant(T, successor, successor->right);
+			//delete_me right child becomes successor right child
+			succcessor->right = delete_me->right;
+			succcessor->right.parent = successor;
+			//replace delete_me by it's successor
+			transplant(T, delete_me, successor);
+			successor->left = delete_me->left;
+			succcessor->left.parent = successor;
+		}
+	}
+}
+
+enum	Color
+{
+	red,
+	black;
+};
+struct	red_black_tree
+{
+	int				key;
+	Color			color;
+	red_black_tree	*left;
+	red_black_tree	*right;
+	red_black_tree	*parent;
+}
+
+//root and NULL leaves are black
+//if node is red it's children are black
+//for each node all simple pathes to nodes below have same number of black
+
+//rotate left
+
+void	left-rotate(T, x)
+{
+	y = x.right;
+	x.right = y.left
+	if (y.left != NULL)
+		y.left.parent = x;
+	y.parent = x.parent;
+	if (x.parent == T.nill) //if x was root, change the root to be y
+		T.root = y;
+	else if (x == x.parent.left) //x was the left child
+		x.parent.left = y; //change the parent of y to be x's parent
+	else	//other wise x was the right child to it's parent
+		x.parent.right = y;
+	y.left = x;
+	x.parent = y;
+}
+//the right rotate is unverified
+// void	right-rotate(T, y)
+// {
+// 	x = y.left;
+// 	y.left = x.right
+// 	if (x.right != NULL)
+// 		x.right.parent = y;
+// 	x.parent = y.parent;
+// 	if (y.parent == T.nill) //if y was root, change the root to be x
+// 		T.root = x;
+// 	else if (y == y.parent.right) //y was the right child
+// 		y.parent.right = x; //change the parent of x to be y's parent
+// 	else	//other wise y was the left child to it's parent
+// 		y.parent.left = x;
+// 	x.right = y;
+// 	y.parent = x;
+// }
+
+//note that all instance of NULL, replaced with T.nill
+//insert in redblack tree
+
+//fixing up the red black tree
+void	red_black_insert_fixup(T, Z)
+{
+	
+}
+void	red_black_insert(T, z)
+{
+	x = T.root //first node to compare with z
+	y = T.nill //y is the parent of z
+	while (x != T.nill)
+	{
+		y = x;
+		//shearching where to insert z
+		if (z.key < x.key)
+			x = x.left;
+		else
+			x = x.right;
+	}
+	z.parent = y; //found location to insert z with parent y
+	if (y == T.nill)
+		T.root = z; //the tree was empty
+	else if (z.key < y.key)
+		y.left = z;
+	else
+		y.right = z;
+	z.left = T.nill;
+	z.right = T.nill;
+	z.color = red;  //the new node start red
+	red_black_insert_fixup(T, Z); //correct any violation in the redblack tree
+}
